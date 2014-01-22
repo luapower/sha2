bce35db7d2c3af6311b6f188676a1a5072c089f8
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:52:41 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/sha2/build-linux32.sh
+++ csrc/sha2/build-linux32.sh
@@ -1,4 +1,4 @@
-gcc $CFLAGS sha2.c -shared -o ../../bin/linux32/libsha2.so -I. \
+gcc -O2 -s -static-libgcc sha2.c -shared -o ../../bin/linux32/libsha2.so -I. \
 	-DSHA2_USE_INTTYPES_H -DBYTE_ORDER -DLITTLE_ENDIAN
 
 #cd ../.. && linux/bin/luajit sha2_test.lua
