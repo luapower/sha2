fc52c6affe8280f13dde5c81c7d1e709ca089524
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:09:26 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/sha2/build-osx32.sh
+++ csrc/sha2/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 sha2.c -shared -o ../../bin/osx32/libsha2.dylib -I. -DSHA2_USE_INTTYPES_H -DBYTE_ORDER -DLITTLE_ENDIAN
+gcc -arch i386 -O2 sha2.c -shared -install_name @loader_path/libsha2.dylib -o ../../bin/osx32/libsha2.dylib -I. -DSHA2_USE_INTTYPES_H -DBYTE_ORDER -DLITTLE_ENDIAN
